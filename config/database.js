2eee9cb78e090a2af5bd1c6b5c70d24f2fbc260e
Steven Schwab <schwabs@umich.edu>
2022-08-28 16:45:50 -0500

    cleaned up config folder

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.DB_STRING)
+        const conn = await mongoose.connect(process.env.DB_STRING);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
-        console.log(err)
-        process.exit(1)
+        console.log(err);
+        process.exit(1);
     }
-}
-// exporting a function
-module.exports = connectDB
+};
+
+module.exports = connectDB;
