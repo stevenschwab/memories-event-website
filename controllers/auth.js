f440188946522cab8ce50c657294448ce05407cd
Steven Schwab <schwabs@umich.edu>
2022-08-31 15:34:12 -0500

    updated auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,10 +45,8 @@ exports.postLogin = (req, res, next) => {
 };
   
 exports.logout = (req, res) => {
-  req.logOut((err) => {
-    if (err) {
-      return next(err);
-    }
+  req.logout(() => {
+    console.log('User has logged out.')
   });
   req.session.destroy((err) => {
     if (err) 
--- routes/createYourOwnEvent.js
+++ routes/createYourOwnEvent.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const upload = require("../middleware/multer");
 const createYourOwnEventController = require('../controllers/createYourOwnEvent');
-const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', ensureAuth, createYourOwnEventController.getEventIndex);
 
