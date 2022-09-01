02da44d3ed999e5795aeb4898f69b5231bf194d2
Steven Schwab <schwabs@umich.edu>
2022-08-31 19:23:31 -0500

    got navbar to stay underlined after click

--- public/js/main.js
+++ public/js/main.js
@@ -16,3 +16,15 @@
         }
     });
 })()
+
+$(document).ready(function () {
+    // Underline to remain in navbar after click using URL
+    jQuery(function ($) {
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+      $('header > nav > ul > li > a').each(function () {
+        if (this.href === path) {
+          $(this).addClass('active');
+        }
+      });
+    });
+});
